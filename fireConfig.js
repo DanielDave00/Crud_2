4f45588fbb0f62746b1373d5bd1e29bd0070d58f
pablo segundo <144213400+DanielDave00@users.noreply.github.com>
2024-05-28 10:17:40 +0800

    Crud

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAONCc6vM31PZcT7BUcWS3G7gsFmndngDE",
+  authDomain: "sign-in-sign-up-81693.firebaseapp.com",
+  databaseURL: "https://sign-in-sign-up-81693-default-rtdb.firebaseio.com",
+  projectId: "sign-in-sign-up-81693",
+  storageBucket: "sign-in-sign-up-81693.appspot.com",
+  messagingSenderId: "441537206202",
+  appId: "1:441537206202:web:aa316982fa72c8a2812f83",
+  measurementId: "G-LMZGVR9XVS"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
